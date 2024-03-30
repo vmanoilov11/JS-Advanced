83b33ccfc0f4498d30ab459eeeae9251692f2bbb
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-30 22:26:31 +0200

    Create dropdown.js

---
+++ 04. Fill-Dropdown/dropdown.js
@@ -0,0 +1,18 @@
+function addItem() {
+
+    let valueElement = document.getElementById("newItemValue");
+    let textElement = document.getElementById("newItemText")
+    let select = document.getElementById("menu");
+
+    let option = document.createElement("option")
+    option.textContent = textElement.value;
+    option.value = valueElement.value;
+
+
+    select.appendChild(option);
+
+    valueElement.value = ''
+    textElement.value = ""
+
+
+}
