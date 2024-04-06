23ed424ff458c019d774cefe5d2aa830de82834e
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-04-06 23:42:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+function lockedProfile() {
+    Array.from(document.querySelectorAll(".profile button")).forEach((btn) => 
+        btn.addEventListener("click", ToggleProfile));
+
+    function ToggleProfile(e){
+    let profile = e.target.parentElement;
+    isUnlocked = profile.querySelector(`input[type="radio"][value="unlock"]`).checked;
+
+    if(isUnlocked){
+        let div = profile.querySelector("div");
+        if(e.target.textContent == "Show more"){
+            div.style.display = "block"
+            e.target.textContent = "Hide it";
+
+        }else{
+            div.style.display = "";
+            e.target.textContent = "Show more";
+
+        }
+    }
+
+    }
+
+}
