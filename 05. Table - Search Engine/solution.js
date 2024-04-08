4a866ea7b2471ae912989b630a81d7eac51bb031
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-04-08 22:57:58 +0300

    Create solution.js

---
+++ 05. Table - Search Engine/solution.js
@@ -0,0 +1,25 @@
+function solve() {
+   document.querySelector('#searchBtn').addEventListener('click', onClick);
+
+   function onClick() {
+      let searchText = document.getElementById('searchField').value.toLowerCase();
+
+      let rows = Array.from(document.querySelectorAll("tbody tr"));
+
+      rows.forEach(row => {
+         row.classList.remove("select");
+      });
+
+      for (let row of rows) {
+         let cells = Array.from(row.children);
+         for (let cell of cells) {
+            if (cell.textContent.toLowerCase().includes(searchText)) {
+               row.classList.add("select");
+               break;
+            }
+         }
+      }
+
+      document.getElementById('searchField').value = '';
+   }
+}
