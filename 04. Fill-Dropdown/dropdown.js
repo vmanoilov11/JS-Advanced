e6c477757c01ba53a639db90f8f9f13313b2f0ab
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-05-10 19:10:19 +0300

    Update dropdown.js

--- 04. Fill-Dropdown/dropdown.js
+++ 04. Fill-Dropdown/dropdown.js
@@ -1,18 +1,18 @@
 function addItem() {
 
     let valueElement = document.getElementById("newItemValue");
-    let textElement = document.getElementById("newItemText")
+    let textElement = document.getElementById("newItemText");
     let select = document.getElementById("menu");
 
-    let option = document.createElement("option")
+    let option = document.createElement("option");
     option.textContent = textElement.value;
     option.value = valueElement.value;
 
 
     select.appendChild(option);
 
-    valueElement.value = ''
-    textElement.value = ""
+    valueElement.value = '';
+    textElement.value = "";
 
 
 }
