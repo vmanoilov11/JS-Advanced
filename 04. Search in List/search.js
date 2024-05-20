65208363b94222e0e55acbd12e7381df31ecebf0
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-05-20 23:37:58 +0300

    Update search.js

--- 04. Search in List/search.js
+++ 04. Search in List/search.js
@@ -1,17 +1,17 @@
 function search() {
    let listElements = document.querySelectorAll("ul#towns li");
    let searchText = document.getElementById("searchText").value;
-   let resultElement = document.getElementById("result")
-   let matches = 0
+   let resultElement = document.getElementById("result");
+   let matches = 0;
    for(let element of listElements) {
-      element.style.fontWeight = "normal"
-      element.style.textDecoration = ""
+      element.style.fontWeight = "normal";
+      element.style.textDecoration = "";
    }
 
    for(let element of listElements) {
       let text = element.textContent
       if(text.match(searchText)){
-         matches++
+         matches++;
          element.style.fontWeight = "bold";
          element.style.textDecoration = "underline";
       }
