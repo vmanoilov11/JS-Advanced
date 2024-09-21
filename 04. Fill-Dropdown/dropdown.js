e0db077b4d6c88f6614768a54280e63c123e2ab3
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-09-21 23:47:08 +0300

    Update dropdown.js

--- 04. Fill-Dropdown/dropdown.js
+++ 04. Fill-Dropdown/dropdown.js
@@ -3,7 +3,6 @@ function addItem() {
     let valueElement = document.getElementById("newItemValue");
     let textElement = document.getElementById("newItemText");
     let select = document.getElementById("menu");
-
     let option = document.createElement("option");
     option.textContent = textElement.value;
     option.value = valueElement.value;
